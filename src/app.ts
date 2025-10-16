98c96c3b752de8fb96e659f2719fdc9267b94f26
nguyenquanghuy12052005 <daotran1252005@gmail.com>
2025-10-16 23:21:16 +0700

    kết nối mongo và middleware

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,73 @@
-
 import express from 'express';
+import mongoose from 'mongoose';
 import { Route } from './core/interface';
+import hpp from 'hpp';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
+import { Logger } from './core/utils';
+
 class App {
-    public app: express.Application;
-    public port: String | Number;
+  public app: express.Application;
+  public port: string | number;
+  public production: Boolean;
 
-    constructor(routes: Route[]) {
-        this.app = express();
-        this.port = process.env.PORT || 5000; 
-        
-        this.initializeRoutes(routes);
-    }  
+  constructor(routes: Route[]) {
+    this.app = express();
+    this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV =="production" ? true : false;
 
+    
+    this.initializeRoutes(routes);
+    this.connectToDatabase(); // Gọi kết nối DB
+    this.initializeMiddleware();
+  }
 
-    public listen() {
-        this.app.listen(this.port, ()=>{
-    console.log("chạy " + this.port)
-}); 
+  //hàm run
+  public listen() {
+    this.app.listen(this.port, () => {
+       Logger.info("Server đang chạy tại cổng " + this.port);
+    });
+  }
+
+  //duyệt hết các route truy cập vào 
+  private initializeRoutes(routes: Route[]) {
+    routes.forEach((route) => {
+      this.app.use('/', route.router);
+    });
+  }
+
+//bảo mật
+  private initializeMiddleware() {
+    if(this.production) {
+        this.app.use(hpp()); //lọc giá trị trùng
+        this.app.use(helmet());
+        this.app.use(morgan('combined'));
+        this.app.use(cors({origin: 'your.domain.com', credentials: true})); //chỉ cho phép domain chỉ định try cập  
+
     }
+    else {
+        this.app.use(morgan('dev'));
+        this.app.use(cors({origin: true, credentials: true}));
 
 
-    private initializeRoutes(routes: Route[]) {
-        routes.forEach((route) => {
-            this.app.use('/', route.router);
-        });
     }
+  }
+
+  // kết nối database
+  private async connectToDatabase() {
+
+         const connectString = process.env.MONGODB_URI;
+         if(!connectString) {
+             Logger.error("connectString invalid");      
+            return;    
+         }
+      await mongoose.connect(connectString).catch((reason) => {
+        Logger.error(reason);
+      });
+      Logger.info("Kết nối MongoDB Atlas thành công!");
+    
+  }
 }
+
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import App from "./app"
+import "dotenv/config";
+import App from "./app";
 import { IndexRoute } from "./modules/index";
+
 
 const routes = [new IndexRoute()];
 const app = new App(routes);
 
-app.listen();
+app.listen(); 
